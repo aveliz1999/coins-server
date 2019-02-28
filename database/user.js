e73643e0131c60bb1a487f6f208258e8f5abddff
Adrian Veliz <aveliz1999@gmail.com>
2019-02-27 19:40:25 -0500

    Created User DAO
    Created database/user.js DAO to manage users in the database

---
+++ database/user.js
@@ -0,0 +1,133 @@
+const mysql = require('./mysql');
+
+/**
+ * Get user by their ID
+ *
+ * @param {Number} id The integer ID to look for
+ * @param {Connection} connection The connection to use for the query. By default retrieves a new one from the connection pool
+ * @returns {Promise} A promise that resolves to the user data if it's successful
+ */
+exports.getById = function(id, connection = mysql.pool) {
+    return new Promise(function(resolve, reject) {
+        connection.query('SELECT id, email, password, BIN_TO_UUID(uuid) AS uuid FROM `user` WHERE `id` = ?', [id], function(err, rows, fields) {
+            if(err) return reject(err);
+            if(rows[0] === undefined) return reject(new Error('User not found'));
+            resolve(rows[0]);
+        });
+    });
+};
+
+/**
+ * Get user by their ID
+ *
+ * @param {String} uuid The UUID string to look for
+ * @param {Connection} connection The connection to use for the query. By default retrieves a new one from the connection pool
+ * @returns {Promise} A promise that resolves to the user data if it's successful
+ */
+exports.getByUuid = function(uuid, connection = mysql.pool) {
+    return new Promise(function(resolve, reject) {
+        connection.query('SELECT id, email, password, BIN_TO_UUID(uuid) AS uuid FROM `user` WHERE `uuid` = UUID_TO_BIN(?)', [uuid], function(err, rows, fields) {
+            if(err) return reject(err);
+            if(rows[0] === undefined) return reject(new Error('User not found'));
+            resolve(rows[0]);
+        });
+    });
+};
+
+/**
+ * Get user by their email
+ *
+ * @param {String} email The email string to look for
+ * @param {Connection} connection The connection to use for the query. By default retrieves a new one from the connection pool
+ * @returns {Promise} A promise that resolves to the user data if it's successful
+ */
+exports.getByEmail = function(email, connection = mysql.pool) {
+    return new Promise(function(resolve, reject) {
+        connection.query('SELECT id, email, password, BIN_TO_UUID(uuid) AS uuid FROM `user` WHERE `email` = ?', [email], function(err, rows, fields) {
+            if(err) return reject(err);
+            if(rows[0] === undefined) return reject(new Error('User not found'));
+            resolve(rows[0]);
+        });
+    });
+};
+
+/**
+ * Get a list of users by their Name
+ *
+ * @param {String} name The name string to look for
+ * @param {Number} previousId The row id to start looking. Used for pagination, and defaults to 0
+ * @param {Number} limit The amount of rows to retrieve. Defaults to 10
+ * @param {String} orderBy How to order the returned rows. Defaults to "name"
+ * @param {Connection} connection The connection to use for the query. By default retrieves a new one from the connection pool
+ * @returns {Promise} A promise that resolves to the user data if it's successful
+ */
+exports.getListByName = function(name, previousId = 0, limit = 10, orderBy = 'name', connection = mysql.pool) {
+    return new Promise(function(resolve, reject) {
+        let query;
+        let parameters;
+        if(previousId === 0){
+            query = 'SELECT id, email, password, BIN_TO_UUID(uuid) AS uuid FROM `user` WHERE `name` = ? ORDER BY ? LIMIT ?';
+            parameters = [name, orderBy, limit];
+        }
+        else{
+            query = 'SELECT id, email, password, BIN_TO_UUID(uuid) AS uuid FROM `user` WHERE `id` > ? AND `name` = ? ORDER BY ? LIMIT ?';
+            parameters = [previousId, name, orderBy, limit];
+        }
+        connection.query(query, parameters, function(err, rows, fields) {
+            if(err) return reject(err);
+            resolve(rows);
+        });
+    });
+};
+
+/**
+ * Create a new user with the given information
+ *
+ * @param {String} email The email to use for the user. Must be unique or it returns an error
+ * @param {String} password The password to use for the user
+ * @param {Connection} connection The connection to use for the query. By default retrieves a new one from the connection pool
+ * @returns {Promise} A promise that resolves to true if the user is created successfully
+ */
+exports.create = function(email, password, connection = mysql.pool) {
+    return new Promise(function(resolve, reject) {
+        connection.query('INSERT INTO `user` (email, password, uuid) VALUES (?, ?, UUID_TO_BIN(UUID()))', [email, password], function(err, result, fields) {
+            if(err) return reject(err);
+            resolve(true);
+        });
+    });
+};
+
+/**
+ * Updates the email of a user with a given ID
+ *
+ * @param {Number} id The ID of the user being updated
+ * @param {String} newEmail The new email to set for the user
+ * @param {Connection} connection The connection to use for the query. By default retrieves a new one from the connection pool
+ * @returns {Promise} A promise that resolves to true if the user is updated successfully
+ */
+exports.updateEmail = function(id, newEmail, connection = mysql.pool) {
+    return new Promise(function(resolve, reject) {
+        connection.query('UPDATE `user` SET `email` = ? WHERE `id` = ?', [newEmail, id], function(err, result, fields) {
+            if(err) return reject(err);
+            resolve(true);
+        });
+    });
+};
+
+/**
+ * Updates the name of a user with a given ID
+ *
+ * @param {Number} id The ID of the user being updated
+ * @param {String} newName The new name to set for the user
+ * @param {Connection} connection The connection to use for the query. By default retrieves a new one from the connection pool
+ * @returns {Promise} A promise that resolves to true if the user is updated successfully
+ */
+exports.updateName = function(id, newName, connection = mysql.pool) {
+    return new Promise(function(resolve, reject) {
+        connection.query('UPDATE `user` SET `name` = ? WHERE `id` = ?', [newName, id], function(err, result, fields) {
+            if(err) return reject(err);
+            resolve(true);
+        });
+    });
+};
+
