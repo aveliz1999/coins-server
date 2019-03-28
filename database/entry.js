993d968d5b810e88aded2180280138d505206284
Adrian Veliz <aveliz1999@gmail.com>
2019-03-27 23:48:03 -0400

    Changed entry queries to use knex, optional columns
    -Entry queries use knex query builder rather than string queries
    -Entry get/find queries have a columns parameter to exclude colums when
    they're not needed
    -Entry find queries have an order parameter for descend/ascend ordering

--- database/entry.js
+++ database/entry.js
@@ -1,15 +1,20 @@
 const mysql = require('./mysql');
+const knex = require('knex')({client: 'mysql'});
 
 /**
  * Get an entry by its ID
  *
  * @param {Number} id The integer ID to look for
+ * @param {String[]} columns The columns to retrieve from the database
  * @param {Connection} connection The connection to use for the query. By default retrieves a new one from the connection pool
  * @returns {Promise} A promise that resolves to the entry data if it's successful
  */
-exports.getById = function (id, connection = mysql.pool) {
+exports.getById = function (id, columns = ['id', 'user', 'coin', 'amount'], connection = mysql.pool) {
     return new Promise(function (resolve, reject) {
-        connection.query('SELECT id, user, coin, amount FROM `entry` WHERE `id` = ?', [id], function (err, rows, fields) {
+        const query = knex('entry')
+            .select(columns)
+            .where('id', id);
+        connection.query(query.toQuery(), function (err, rows, fields) {
             if (err) return reject(err);
             if (rows[0] === undefined) return reject(new Error('Entry not found'));
             resolve(rows[0]);
@@ -22,12 +27,17 @@ exports.getById = function (id, connection = mysql.pool) {
  *
  * @param {Number} coinId The coin it belongs to
  * @param {Number} userId The user it belongs to
+ * @param {String[]} columns The columns to retrieve from the database
  * @param {Connection} connection The connection to use for the query. By default retrieves a new one from the connection pool
  * @returns {Promise} A promise that resolves to the entry data if it's successful
  */
-exports.getByCoinAndUser = function(coinId, userId, connection = mysql.pool) {
+exports.getByCoinAndUser = function(coinId, userId, columns = ['id', 'user', 'coin', 'amount'], connection = mysql.pool) {
     return new Promise(function(resolve, reject) {
-        connection.query('SELECT * FROM `entry` WHERE coin = ? AND user = ?', [coinId, userId], function(err, rows, fields) {
+        const query = knex('entry')
+            .select(columns)
+            .where('coin', coinId)
+            .where('user', userId);
+        connection.query(query.toQuery(), [coinId, userId], function(err, rows, fields) {
             if (err) return reject(err);
             if (rows[0] === undefined) return reject(new Error('Entry not found'));
             resolve(rows[0]);
@@ -42,21 +52,22 @@ exports.getByCoinAndUser = function(coinId, userId, connection = mysql.pool) {
  * @param {Number} previousId The row id to start looking. Used for pagination, and defaults to 0
  * @param {Number} limit The amount of rows to retrieve. Defaults to 10
  * @param {String} orderBy How to order the returned rows. Defaults to "amount"
+ * @param {String} order The way to order the returned rows. Defaults to "desc", or descending
+ * @param {String[]} columns The columns to retrieve from the database
  * @param {Connection} connection The connection to use for the query. By default retrieves a new one from the connection pool
  * @returns {Promise} A promise that resolves to a list of entry data if it's successful
  */
-exports.getListByUser = function (userId, previousId = 0, limit = 10, orderBy = 'amount', connection = mysql.pool) {
+exports.getListByUser = function (userId, previousId = 0, limit = 10, orderBy = 'amount', order = 'desc', columns = ['id', 'user', 'coin', 'amount'], connection = mysql.pool) {
     return new Promise(function (resolve, reject) {
-        let query;
-        let parameters;
-        if (previousId === 0) {
-            query = 'SELECT * FROM `entry` WHERE `user` = ? ORDER BY ? LIMIT ?';
-            parameters = [userId, orderBy, limit];
-        } else {
-            query = 'SELECT * FROM `entry` WHERE `id` > ? AND `user` = ? ORDER BY ? LIMIT ?';
-            parameters = [previousId, userId, orderBy, limit];
+        let query = knex('entry')
+            .select(columns)
+            .where('user', userId)
+            .orderBy(orderBy, order)
+            .limit(limit);
+        if(previousId > 0){
+            query = query.where('id', '>', previousId)
         }
-        connection.query(query, parameters, function (err, rows, fields) {
+        connection.query(query.toQuery(), function (err, rows, fields) {
             if (err) return reject(err);
             resolve(rows);
         });
@@ -70,21 +81,22 @@ exports.getListByUser = function (userId, previousId = 0, limit = 10, orderBy = 
  * @param {Number} previousId The row id to start looking. Used for pagination, and defaults to 0
  * @param {Number} limit The amount of rows to retrieve. Defaults to 10
  * @param {String} orderBy How to order the returned rows. Defaults to "amount"
+ * @param {String} order The way to order the returned rows. Defaults to "desc", or descending
+ * @param {String[]} columns The columns to retrieve from the database
  * @param {Connection} connection The connection to use for the query. By default retrieves a new one from the connection pool
  * @returns {Promise} A promise that resolves to a list of entry data if it's successful
  */
-exports.getListByCoin = function (coinId, previousId = 0, limit = 10, orderBy = 'amount', connection = mysql.pool) {
+exports.getListByCoin = function (coinId, previousId = 0, limit = 10, orderBy = 'amount', order = 'desc', columns = ['id', 'user', 'coin', 'amount'], connection = mysql.pool) {
     return new Promise(function (resolve, reject) {
-        let query;
-        let parameters;
-        if (previousId === 0) {
-            query = 'SELECT * FROM `entry` WHERE `coin` = ? ORDER BY ? LIMIT ?';
-            parameters = [coinId, orderBy, limit];
-        } else {
-            query = 'SELECT * FROM `entry` WHERE `id` > ? AND `coin` = ? ORDER BY ? LIMIT ?';
-            parameters = [previousId, coinId, orderBy, limit];
+        let query = knex('entry')
+            .select(columns)
+            .where('coin', coinId)
+            .orderBy(orderBy, order)
+            .limit(limit);
+        if(previousId > 0){
+            query = query.where('id', '>', previousId)
         }
-        connection.query(query, parameters, function (err, rows, fields) {
+        connection.query(query.toQuery(), function (err, rows, fields) {
             if (err) return reject(err);
             resolve(rows);
         });
@@ -102,7 +114,13 @@ exports.getListByCoin = function (coinId, previousId = 0, limit = 10, orderBy = 
  */
 exports.create = function (userId, coinId, amount = 0, connection = mysql.pool) {
     return new Promise(function (resolve, reject) {
-        connection.query('INSERT INTO `entry` (user, coin, amount) VALUES (?, ?, ?)', [userId, coinId, amount], function (err, result, fields) {
+        const query = knex('entry')
+            .insert({
+                user: userId,
+                coin: coinId,
+                amount: amount
+            });
+        connection.query(query.toQuery(), function (err, result, fields) {
             if (err) return reject(err);
             resolve(result.insertId);
         });
@@ -119,7 +137,10 @@ exports.create = function (userId, coinId, amount = 0, connection = mysql.pool) 
  */
 exports.updateAmount = function (id, newAmount, connection = mysql.pool) {
     return new Promise(function (resolve, reject) {
-        connection.query('UPDATE `entry` SET `amount` = ? WHERE `id` = ?', [newAmount, id], function (err, result, fields) {
+        const query = knex('entry')
+            .where('id', id)
+            .update({amount: newAmount});
+        connection.query(query.toQuery(), function (err, result, fields) {
             if (err) return reject(err);
             resolve(id);
         });
